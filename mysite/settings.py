549ba3d34ea0d3d6f609448f49a6e8674f37a4cd
Johanna <johannarios.es@gmail.com>
2026-05-19 13:10:34 -0500

    Cambio de Debug.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7ed=rv73y7)$*dlymu%=-t$7ciq)*z3h)rvm4&hf#60qdgcsb1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
